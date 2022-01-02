6f42651919fde89ba989c23026393cf9df5838bf
Mert Barut <mbarut@c2s12d59.42wolfsburg.de>
2022-01-02 17:03:07 +0100

    started day01

---
+++ day01.py
@@ -0,0 +1,41 @@
+# ex01
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('Anothor line\n')
+
+# ex02
+
+print("I could have code like this.") # comment ignored
+
+# print("This won't run.")
+
+print("This will run.\n")
+
+# ex03
+
+print("I will now count my chickens:\n")
+
+print('Hens', 25.0 + 30.0 / 6)
+print('Rooster', 100.0 - 25.0 * 3.0 % 4.0, '\n')
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+print("Is it true that 3 + 2 < 5 - 7?\n")
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
+
+d = 5
+print(d * 2 * 3.14)
